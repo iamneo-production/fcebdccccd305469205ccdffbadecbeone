fc1283359d83d474ef5abf746266107fb1438796
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:35:51 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+  age: number = 0;
+  ageGroup: string = '';
+
+  checkAge(): void {
+    if (this.age < 0 || this.age > 120) {
+      this.ageGroup = 'Invalid age';
+    } else if (this.age < 1) {
+      this.ageGroup = 'Foetus Infancy';
+    } else if (this.age < 3) {
+      this.ageGroup = 'Toddler years';
+    } else if (this.age < 12) {
+      this.ageGroup = 'Childhood';
+    } else if (this.age < 18) {
+      this.ageGroup = 'Teenage';
+    } else if (this.age < 65) {
+      this.ageGroup = 'Adult';
+    } else if (this.age >= 65) {
+      this.ageGroup = 'Senior citizen';
+    }
+  }
 }
